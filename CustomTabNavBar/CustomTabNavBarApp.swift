91c9215586574c9edfdac848a83f796ec64d8e50
MindXDesigns <mindxdesings@gmail.com>
2022-09-17 13:49:28 -0400

    Initial Commit

---
+++ CustomTabNavBar/CustomTabNavBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabNavBarApp.swift
+//  CustomTabNavBar
+//
+//  Created by Alyce Bittar on 9/17/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabNavBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
